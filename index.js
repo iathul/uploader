5dec5aa4424388cffa5192eadedfa51367d09923
Athul Joy <a.athuljoy@gmail.com>
2021-10-02 12:50:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const httpServer = http.createServer();
+httpServer.on("listening", () => console.log("Listening...."));
+httpServer.on("request", (req, res) => {
+
+    if(req.url === "/"){
+        res.end(fs.readFileSync("index.html"));
+        return;
+    }
+
+    if (req.url === "/upload") {
+        const fileName = req.headers["file-name"];
+        req.on("data", chunk => {
+            fs.appendFileSync(fileName, chunk)
+            console.log(`received chunk! ${chunk.length}`)
+        })
+        res.end("uploaded!")
+    }
+
+})
+
+httpServer.listen(8100);
