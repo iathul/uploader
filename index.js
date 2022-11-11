59ea3ca75db75a9512c78e6fed6363e7c9c932aa
Athul <a.athuljoy@gmail.com>
2022-11-11 19:39:37 +0530

    Add express server

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const http = require("http");
-const fs = require("fs");
-const httpServer = http.createServer();
-httpServer.on("listening", () => console.log("Listening...."));
-httpServer.on("request", (req, res) => {
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+app.use(express.static(path.join(__dirname, 'public')))
 
-    if(req.url === "/"){
-        res.end(fs.readFileSync(__dirname +'/client/index.html'));
-        return;
-    }
-
-    if (req.url === "/upload") {
-        const fileName = req.headers["file-name"];
-        req.on("data", chunk => {
-            fs.appendFileSync(fileName, chunk)
-            console.log(`received chunk! ${chunk.length}`)
-        })
-        res.end("uploaded!")
-    }
-
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
 })
 
-httpServer.listen(8100);
+app.get('/', (req, res) => {
+  const fileName = req.headers['file-name']
+  req.on('data', chunk => {
+    fs.appendFileSync(fileName, chunk)
+    console.log(`received chunk! ${chunk.length}`)
+  })
+  res.end('uploaded!')
+})
+
+PORT = 8100
+app.listen(PORT, () => console.log('Server started '))
