b5b21328a0319b7090566d6a1ca58b933a36a2db
Athul <a.athuljoy@gmail.com>
2022-11-16 16:13:06 +0530

    Code refactor

--- public/js/script.js
+++ public/js/script.js
@@ -1,154 +1,28 @@
-// File Upload
-function ekUpload() {
-  function Init() {
-    console.log('Upload Initialised')
-
-    var fileSelect = document.getElementById('file-upload'),
-      fileDrag = document.getElementById('file-drag'),
-      submitButton = document.getElementById('submit-button')
-
-    fileSelect.addEventListener('change', fileSelectHandler, false)
-
-    // Is XHR2 available?
-    var xhr = new XMLHttpRequest()
-    if (xhr.upload) {
-      // File Drop
-      fileDrag.addEventListener('dragover', fileDragHover, false)
-      fileDrag.addEventListener('dragleave', fileDragHover, false)
-      fileDrag.addEventListener('drop', fileSelectHandler, false)
-    }
-  }
-
-  function fileDragHover(e) {
-    var fileDrag = document.getElementById('file-drag')
-
-    e.stopPropagation()
-    e.preventDefault()
-
-    fileDrag.className =
-      e.type === 'dragover' ? 'hover' : 'modal-body file-upload'
-  }
-
-  function fileSelectHandler(e) {
-    // Fetch FileList object
-    var files = e.target.files || e.dataTransfer.files
-
-    // Cancel event and hover styling
-    fileDragHover(e)
-
-    // Process all File objects
-    for (var i = 0, f; (f = files[i]); i++) {
-      parseFile(f)
-      uploadFile(f)
-    }
-  }
-
-  // Output
-  function output(msg) {
-    // Response
-    var m = document.getElementById('messages')
-    m.innerHTML = msg
-  }
-
-  function parseFile(file) {
-    console.log(file.name)
-    output('<strong>' + encodeURI(file.name) + '</strong>')
-
-    // var fileType = file.type;
-    // console.log(fileType);
-    var imageName = file.name
-
-    var isGood = /\.(?=gif|jpg|png|jpeg)/gi.test(imageName)
-    if (isGood) {
-      document.getElementById('start').classList.add('hidden')
-      document.getElementById('response').classList.remove('hidden')
-      document.getElementById('notimage').classList.add('hidden')
-      // Thumbnail Preview
-      document.getElementById('file-image').classList.remove('hidden')
-      document.getElementById('file-image').src = URL.createObjectURL(file)
-    } else {
-      document.getElementById('file-image').classList.add('hidden')
-      document.getElementById('notimage').classList.remove('hidden')
-      document.getElementById('start').classList.remove('hidden')
-      document.getElementById('response').classList.add('hidden')
-      document.getElementById('file-upload-form').reset()
-    }
-  }
-
-  function setProgressMaxValue(e) {
-    var pBar = document.getElementById('file-progress')
-
-    if (e.lengthComputable) {
-      pBar.max = e.total
-    }
-  }
-
-  function updateFileProgress(e) {
-    var pBar = document.getElementById('file-progress')
-
-    if (e.lengthComputable) {
-      pBar.value = e.loaded
-    }
-  }
-
-  function uploadFile(file) {
-    var xhr = new XMLHttpRequest(),
-      fileInput = document.getElementById('class-roster-file'),
-      pBar = document.getElementById('file-progress'),
-      fileSizeLimit = 1024 // In MB
-    if (xhr.upload) {
-      // Check if file is less than x MB
-      if (file.size <= fileSizeLimit * 1024 * 1024) {
-        // Progress bar
-        pBar.style.display = 'inline'
-        xhr.upload.addEventListener('loadstart', setProgressMaxValue, false)
-        xhr.upload.addEventListener('progress', updateFileProgress, false)
-
-        // File received / failed
-        xhr.onreadystatechange = function (e) {
-          if (xhr.readyState == 4) {
-            // Everything is good!
-            // progress.className = (xhr.status == 200 ? "success" : "failure");
-            // document.location.reload(true);
-          }
-        }
-
-        // Start upload
-        xhr.open(
-          'POST',
-          document.getElementById('file-upload-form').action,
-          true
-        )
-        xhr.setRequestHeader('X-File-Name', file.name)
-        xhr.setRequestHeader('X-File-Size', file.size)
-        xhr.setRequestHeader('Content-Type', 'multipart/form-data')
-        xhr.send(file)
-      } else {
-        output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).')
-      }
-    }
-  }
-
-  // Check for the various File API support.
-  if (window.File && window.FileList && window.FileReader) {
-    Init()
-  } else {
-    document.getElementById('file-drag').style.display = 'none'
-  }
-}
-ekUpload()
-
 const upload = document.getElementById('upload')
 const output = document.getElementById('output')
-const file = document.getElementById('file')
+var fileSelect = document.getElementById('file-upload')
+var newFile
+fileSelect.addEventListener('change', e => {
+  newFile = e.target.files[0]
+  if (newFile) {
+    document.getElementById('start').classList.add('hidden')
+    document.getElementById('response').classList.remove('hidden')
+    document.getElementById('file-image').classList.remove('hidden')
+    document.getElementById('file-image').src = URL.createObjectURL(newFile)
+    document.getElementById('notimage').classList.add('hidden')
+    document.getElementById('messages').innerHTML = `<strong> ${encodeURI(
+      newFile.name
+    )} </strong>`
+  }
+})
 
-upload.addEventListener('click', () => {
+upload.addEventListener('click', e => {
+  e.preventDefault()
+  document.getElementById('progress-grp').classList.remove('hidden')
   const fileReader = new FileReader()
-  const newFile = file.files[0]
   fileReader.onload = async event => {
     const CHUNK_SIZE = 1000
-    const chunkCount = event.target.result.byteLength / CHUNK_SIZE
-    console.log('Read successfully')
+    const chunkCount = Math.round(event.target.result.byteLength / CHUNK_SIZE,0)
     const fileName = Math.random() * 1000 + newFile.name
     for (let chunkId = 0; chunkId < chunkCount + 1; chunkId++) {
       const chunk = event.target.result.slice(
@@ -164,9 +38,14 @@ upload.addEventListener('click', () => {
         },
         body: chunk
       })
-      output.textContent = Math.round((chunkId * 100) / chunkCount, 0) + '%'
+      console.log(Math.round((chunkId * 100) / chunkCount, 0))
+      document.getElementById('file-progress').value = Math.round(
+        (chunkId * 100) / chunkCount,
+        0
+      )
+      document.getElementById('progress-text').textContent =
+        Math.round((chunkId * 100) / chunkCount, 0) + '%'
     }
-    console.log(event.target.result.byteLength)
   }
   fileReader.readAsArrayBuffer(newFile)
 })
