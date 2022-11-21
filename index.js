b2e59a7c0cdec9190a5af97fa0385d57cea61199
Athul <a.athuljoy@gmail.com>
2022-11-21 21:55:05 +0530

    Add package cloudinary

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fs = require('fs')
+const cloudinary = require('cloudinary')
+
+cloundinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -10,7 +17,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-app.post('/upload', (req, res) => {
+app.post('/upload', async (req, res) => {
   const fileName = req.headers['file-name']
   const fileDir = `./files`
   if (!fs.existsSync(fileDir)) {
@@ -19,6 +26,7 @@ app.post('/upload', (req, res) => {
   req.on('data', chunk => {
     fs.appendFileSync(`${fileDir}/${fileName}`, chunk)
   })
+
   return res.status(200).json({
     message: 'Chunk received successfully.'
   })
