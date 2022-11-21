2fdc09ed1d6686ddc50c41935159e8f57667ad32
Athul <a.athuljoy@gmail.com>
2022-11-21 21:55:49 +0530

    Fix typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const fs = require('fs')
 const cloudinary = require('cloudinary')
 
-cloundinary.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECRET
